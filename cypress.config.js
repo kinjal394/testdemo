7a4039d08035b021a6c79c5d7ba9276267c4f7e8
iam dobariya <iamdobariya@gmail.com>
2023-03-28 14:13:16 -0400

    added mochaosome report

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,38 @@
-const { defineConfig } = require("cypress");
+/*const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+  },
+});
+*/
+
+const { defineConfig } = require('cypress');
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
+
+module.exports = defineConfig({
+  projectId: '85apck',
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      on('before:run', async (details) => {
+        console.log('override before:run');
+        await beforeRunHook(details);
+      });
+
+      on('after:run', async () => {
+        console.log('override after:run');
+        await afterRunHook();
+      });
     },
   },
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,4 +2,8 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
+
+  it('fail', () => {
+    expect(true).to.have(false);
+  })
 })
